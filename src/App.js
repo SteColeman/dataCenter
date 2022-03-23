e155bc09169b0ad924508e31d1d3ebaacbd1ddd1
stecoleman <stecoleman1@gmail.com>
2022-03-23 17:02:47 +0000

    component build and rendered for card display with basic css for test purposes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './Components/Card.js'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  //array is temperory until api is added
+  const cardData = [
+    {
+    "title" : "title", 
+    "imageURL" : "url", 
+    "description" : "description"
+  },
+  {
+    "title" : "title 1", 
+    "imageURL" : "url 1", 
+    "description" : "description 1"
+  },
+  {
+    "title" : "title 2", 
+    "imageURL" : "url 2", 
+    "description" : "description 2"
+  }
+  ];
+
+  /*
+  *  loop through the api json array data and push data to relevant variables. 
+  *  push whole card COMPONENT to 'cards' array for react to render all.
+  */
+    var cards = [];
+    for(var i=0; i<cardData.length; i++) {
+      cards.push(
+        <Card
+        title = {cardData[i].title}
+        imageUrl = {cardData[i].imageUrl}
+        description = {cardData[i].description}
+        />
+      )
+    }
+
+
+    //return all elements to the DOM
+    return (
+      <div>
+        {cards}
+      </div>
+      
+    )
 }
+
 
 export default App;
